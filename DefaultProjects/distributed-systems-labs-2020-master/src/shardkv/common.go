ec671a9e6633595597258345a99b4e0e3f2adee6
Giga <Khizanag@gmail.com>
2020-12-18 15:30:23 +0400

    Update after Raft

--- DefaultProjects/distributed-systems-labs-2020-master/src/shardkv/common.go
+++ DefaultProjects/distributed-systems-labs-2020-master/src/shardkv/common.go
@@ -1,8 +1,18 @@
-package kvraft
+package shardkv
+
+//
+// Sharded key/value server.
+// Lots of replica groups, each running op-at-a-time paxos.
+// Shardmaster decides which group serves each shard.
+// Shardmaster may change shard assignment from time to time.
+//
+// You will have to modify these definitions.
+//
 
 const (
 	OK             = "OK"
 	ErrNoKey       = "ErrNoKey"
+	ErrWrongGroup  = "ErrWrongGroup"
 	ErrWrongLeader = "ErrWrongLeader"
 )
 
@@ -10,6 +20,7 @@ type Err string
 
 // Put or Append
 type PutAppendArgs struct {
+	// You'll have to add definitions here.
 	Key   string
 	Value string
 	Op    string // "Put" or "Append"
